732aa6ebf5660d319649944a287be46ba59e0e77
IvanQGervas <ivanqgervas@gmail.com>
2021-07-23 13:36:17 +0200

    Añadidos comentarios en el JS

--- jsapp.js
+++ jsapp.js
@@ -1,10 +1,12 @@
 let lista = document.createElement('ul')
 document.body.appendChild(lista)
 
+// Url de la api para el fetch
 const urlBase = 'https://fakestoreapi.com/products';
 
+// Creación de la lista con todos los titulos de productos
 fetch(urlBase).then(data => data.json()).then(json => {
-
+    // Iteramos el objeto devuelto por la api
     json.map(element => {
         let li = document.createElement('li');
         li.appendChild(document.createTextNode(element.title));
@@ -14,11 +16,37 @@ fetch(urlBase).then(data => data.json()).then(json => {
     )
 })
 
+
+
+// Función encargada de pintar todos los productos de una categoria
+let cambio = (value) => {
+    
+    fetch(`${urlBase}/category/${value}`)
+    .then(res => res.json())
+    .then(json => {
+        lista.innerHTML = '';
+        json.map(element => {
+            element
+            lista.innerHTML += `
+            <div class="container">
+            <img class="image" src="${element.image}" />
+            <h2 class="tittle">${element.tittle}</h2>
+            <p class="price">${element.price} €!</p>
+            <p class="description">${element.description}</p>
+            </div>
+            `;
+        })
+    })
+}
+
+
+
+// Creamos un select con todas las categorias como option, este select tiene un onchange que lanza la función cambio.
 fetch(`${urlBase}/categories`)
     .then(res => res.json())
     .then(json => {
         let select = document.createElement('select')
-        select.setAttribute('onchange', "cambio(this.value)");
+        select.setAttribute('onchange', "cambio(this.value)"); // Aqui insertamos la función como atributo on change.
         document.body.appendChild(select);
 
         let option = document.createElement('option');
@@ -37,25 +65,3 @@ fetch(`${urlBase}/categories`)
         console.log(select.value);
 
     })
-
-
-
-let cambio = (value) => {
-
-    fetch(`${urlBase}/category/${value}`)
-        .then(res => res.json())
-        .then(json => {
-            lista.innerHTML = '';
-            json.map(element => {
-                element
-                lista.innerHTML += `
-                    <div class="container">
-                    <img class="image" src="${element.image}" />
-                    <h2 class="tittle">${element.tittle}</h2>
-                    <p class="price">${element.price} €</p>
-                    <p class="description">${element.description}</p>
-                    </div>
-                    `;
-            })
-        })
-}
